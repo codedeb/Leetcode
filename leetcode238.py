7032404ce977d049de00a7c1e5691398476a3178
Debashish chakraborty <paganihyura@gmail.com>
2019-12-17 13:14:42 +0530

    few changes

---
+++ leetcode238.py
@@ -0,0 +1,12 @@
+
+def productExceptSelf(nums):
+    newlist = []
+    total = numpy.prod(nums)
+    for i in range(len(nums)):
+        newlist.append(total//nums[i])
+
+    return newlist
+
+
+a = productExceptSelf([1, 2, 3, 4])
+print(a)
